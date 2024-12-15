e4426c8e6bbb9431c38bd6f6a03f1067ea8edab7
sarangnath <sarangnadhkayyur@gmail.com>
2024-12-15 23:21:25 +0530

    updates

--- app/_components/Status.jsx
+++ app/_components/Status.jsx
@@ -1,41 +1,38 @@
-"use client"
+"use client";
 import { motion } from "framer-motion";
+import CountUp from "react-countup";
 
-import CountUp from "react-countup"
-
-const stats =[
-    {
-        num:720,
-        text:"Projects Committed"
-    },
-    {
-        num:520,
-        text:"Happy Homes"
-    },
-    
-    {
-        num:60 ,
-        text:"Cities"
-    },
-  
-    {
-        num:12,
-        text:"Countries"
-    }
-]
+const stats = [
+  {
+    num: 720,
+    text: "Projects Committed",
+  },
+  {
+    num: 520,
+    text: "Happy Homes",
+  },
+  {
+    num: 60,
+    text: "Cities",
+  },
+  {
+    num: 12,
+    text: "Countries",
+  },
+];
 
 const Stats = () => {
   return (
-    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0 bg-slate-300 mt-3">
-      <h1 className="text-3xl font-bold sm:text-4xl text-center text-sky-600 mb-6">
+    <section className="pt-6 pb-12 xl:py-16 bg-slate-300 mt-3">
+      <h1 className="text-2xl font-bold sm:text-3xl lg:text-4xl text-center text-sky-600 mb-8">
         Why Choose Us
       </h1>
-      <div className="container mx-auto">
-        <div className="flex flex-wrap gap-6 justify-center max-w-[80vw] mx-auto xl:max-w-none">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-4 justify-center max-w-[90vw] lg:max-w-[75vw] mx-auto">
           {stats.map((item, index) => (
             <motion.div
               key={index}
-              className="flex flex-col items-center text-center gap-2 bg-black shadow-lg rounded-lg p-4 transform transition duration-500 hover:scale-105"
+              className="flex flex-col items-center text-center gap-3 bg-white shadow-lg rounded-lg p-6 transform transition duration-500 hover:scale-105"
               initial={{ opacity: 0, y: 50 }}
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: index * 0.2 }}
@@ -49,13 +46,13 @@ const Stats = () => {
                   end={item.num}
                   duration={3}
                   delay={2}
-                  className="text-4xl xl:text-6xl font-extrabold text-sky-600"
+                  className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-sky-600"
                 />
               </motion.div>
               <p
                 className={`${
                   item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
-                } text-lg leading-snug text-gray-700`}
+                } text-sm sm:text-base md:text-lg leading-snug text-gray-700`}
               >
                 {item.text}
               </p>
@@ -64,7 +61,7 @@ const Stats = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Stats
+export default Stats;
